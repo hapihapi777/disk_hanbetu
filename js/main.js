b2815b0a9faad4b093ed2628cdafa736f0c28a90
hapi2 <rock666gemini69@gmail.com>
2021-04-18 11:47:27 +0900

    ナビ無しリプの考慮するしないの判定をラジオボタンで実装。

--- js/main.js
+++ js/main.js
@@ -35,28 +35,46 @@
     let result = [];
     let hiritu = [];
     let goukei = 0;
-    for (let i = 0; i < dankai; i++) {
-      hiritu.push(
-        CalcBinom(soukaiten, cherry, cherry_kakuritu)[i] // チェリーで期待値
-        * CalcBinom(soukaiten, suika, suika_kakuritu)[i] // スイカで期待値
-        * CalcBinom(art, hosi, hosi_kakuritu)[i] // art中の星で期待値
-        * CalcBinom(art, hazure, hazure_kakuritu)[i] // art中ハズレで期待値
-        * CalcBinom(art, rep, nabinasi_kakuritu)[i] // art中の星で期待値
-      );
-      goukei += hiritu[i];
+
+    let target = document.getElementById("target"); //ラジオボタン
+    let s = target.hoge.value;
+    
+    if (s === 'nouse') {
+      for (let i = 0; i < dankai; i++) {
+        hiritu.push(
+          CalcBinom(soukaiten, cherry, cherry_kakuritu)[i] // チェリーで期待値
+          * CalcBinom(soukaiten, suika, suika_kakuritu)[i] // スイカで期待値
+          * CalcBinom(art, hosi, hosi_kakuritu)[i] // art中の星で期待値
+          * CalcBinom(art, hazure, hazure_kakuritu)[i] // art中ハズレで期待値
+          // * CalcBinom(art, rep, nabinasi_kakuritu)[i] // art中ナビなしリプで期待値
+        );
+        goukei += hiritu[i];
+      }
+    } else {
+      for (let i = 0; i < dankai; i++) {
+        hiritu.push(
+          CalcBinom(soukaiten, cherry, cherry_kakuritu)[i] // チェリーで期待値
+          * CalcBinom(soukaiten, suika, suika_kakuritu)[i] // スイカで期待値
+          * CalcBinom(art, hosi, hosi_kakuritu)[i] // art中の星で期待値
+          * CalcBinom(art, hazure, hazure_kakuritu)[i] // art中ハズレで期待値
+          * CalcBinom(art, rep, nabinasi_kakuritu)[i] // art中ナビなしリプで期待値
+        );
+        goukei += hiritu[i];
+      }
+
     }
 
     hiritu.forEach(e => {
       result.push(Math.round((parseFloat(e / goukei * 100)) * 100) / 100);
     });
 
-    console.log("ちょり" + CalcBinom(soukaiten, cherry, cherry_kakuritu));
-    console.log("すいか" + CalcBinom(soukaiten, suika, suika_kakuritu));
-    console.log("ほし" + CalcBinom(art, hosi, hosi_kakuritu));
-    console.log("はずれ" + CalcBinom(art, hazure, hazure_kakuritu));
-    console.log("りぷ" + CalcBinom(art, rep, nabinasi_kakuritu));
-    console.log(hiritu);
-    console.log(goukei);
+    // console.log("ちょり" + CalcBinom(soukaiten, cherry, cherry_kakuritu));
+    // console.log("すいか" + CalcBinom(soukaiten, suika, suika_kakuritu));
+    // console.log("ほし" + CalcBinom(art, hosi, hosi_kakuritu));
+    // console.log("はずれ" + CalcBinom(art, hazure, hazure_kakuritu));
+    // console.log("りぷ" + CalcBinom(art, rep, nabinasi_kakuritu));
+    // console.log(hiritu);
+    // console.log(goukei);
     // console.log(art);
 
     document.getElementById('r_soukaiten').textContent = " " + soukaiten + "G";
@@ -106,7 +124,7 @@
 
     for (let i = 0; i < dankai; i++) {
       koyaku_kakuritu_p.push(BigInt(Math.floor(1 / array[i] * accuracy)));
-      console.log(koyaku_kakuritu_p[i]);
+      // console.log(koyaku_kakuritu_p[i]);
     }
 
     return koyaku_kakuritu_p;
@@ -139,18 +157,20 @@
     Button_Keisan('rep_btn', 'rep', 1);// リプボタン
 
     // dtボタンで総artに追加する(その後dt入力欄は0)
-    document.getElementById('dt_btn').addEventListener('click', function () {
+    document.getElementById('tuika_btn').addEventListener('click', function () {
       document.getElementById('sou_art').value = parseInt(document.getElementById('sou_art').value) +
-        parseInt(document.getElementById('dt_art').value);
-      document.getElementById('dt_art').value = 0;
+        parseInt(document.getElementById('dt_art').value) +
+        parseInt(document.getElementById('djz_art').value);
+        document.getElementById('dt_art').value = 0;
+        document.getElementById('djz_art').value = 0;
     });
 
-    // djzボタンで総artに追加する(その後djz入力欄は0)
-    document.getElementById('djz_btn').addEventListener('click', function () {
-      document.getElementById('sou_art').value = parseInt(document.getElementById('sou_art').value) +
-        parseInt(document.getElementById('djz_art').value);
-      document.getElementById('djz_art').value = 0;
-    });
+    // // djzボタンで総artに追加する(その後djz入力欄は0)
+    // document.getElementById('djz_btn').addEventListener('click', function () {
+    //   document.getElementById('sou_art').value = parseInt(document.getElementById('sou_art').value) +
+    //     parseInt(document.getElementById('djz_art').value);
+    //   document.getElementById('djz_art').value = 0;
+    // });
 
   }
 
@@ -164,13 +184,22 @@
   function GetRadio() {
       let target = document.getElementById("target");
       let s = target.hoge.value;
+      
       // console.log(s);
-      if(s === 'use') {
-        document.getElementById('rep_btn').disabled = false;
-      }else if(s === 'nouse') {
-        document.getElementById('rep_btn').disabled = true;
+      // if(s === 'use') {
+      //   document.getElementById('rep_btn').disabled = false;
+      // }else if(s === 'nouse') {
+      //   document.getElementById('rep_btn').disabled = true;
+      // }
+      // let s;
+      if (target.hoge.checked) {
+        // s = target.hoge.value;
+        console.log(s);
       }
+
       // return s;
   }
+
+
 
 }
